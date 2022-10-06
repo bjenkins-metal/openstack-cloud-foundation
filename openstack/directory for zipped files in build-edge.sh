14252690ada50fc895c0560ea383a7a332ee6f12
Benjamin Jenkins <74058939+bjenkins-metal@users.noreply.github.com>
2022-10-06 12:16:29 -0500

    add description of directory

---
+++ openstack/directory for zipped files in build-edge.sh
@@ -0,0 +1,2 @@
+The file https://github.com/bjenkins-metal/openstack-cloud-foundation/blob/main/build-edge.sh contains 4 zipped and base64 encoded files.
+This directory contains the readable versoions of those files
