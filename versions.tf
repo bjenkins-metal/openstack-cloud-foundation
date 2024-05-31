b499f298a9a179aba23dd7273a62d11ffb2499fd
Marques Johansson <mjohansson@equinix.com>
2024-05-31 13:59:03 -0400

    set module_name in Equinix provider_meta
    The module_name will help identify usage of this module.

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+
+  provider_meta "equinix" {
+    module_name = "openstack-cloud-foundation"
+  }
+
   required_providers {
     equinix = {
       source  = "equinix/equinix"
