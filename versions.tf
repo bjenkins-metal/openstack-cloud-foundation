7c434adccfc6e2f2c35651e4a1a6619a66f1f682
Administrator <a@a.a>
2024-08-06 10:57:40 -0500

    Changed provider info

--- versions.tf
+++ versions.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = ">= 1.0"
+
+  provider_meta "equinix" {
+    module_name = "openstack-cloud-foundation"
+  }
+
+
   required_providers {
     equinix = {
       source  = "equinix/equinix"
